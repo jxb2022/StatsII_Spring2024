38c70a8852ac4bfde12760fcbaa4ac67baef9dbf
Jacqueline Bouvier <bouvierj@tcd.ie>
2024-04-12 15:07:54 +0100

    ps4 submit

---
+++ problemSets/PS04/my_answers/jbPS4.R
@@ -0,0 +1,41 @@
+
+# remove objects
+rm(list=ls())
+# detach all libraries
+detachAllPackages <- function() {
+  basic.packages <- c("package:stats", "package:graphics", "package:grDevices", "package:utils", "package:datasets", "package:methods", "package:base")
+  package.list <- search()[ifelse(unlist(gregexpr("package:", search()))==1, TRUE, FALSE)]
+  package.list <- setdiff(package.list, basic.packages)
+  if (length(package.list)>0)  for (package in package.list) detach(package,  character.only=TRUE)
+}
+detachAllPackages()
+
+# load libraries
+pkgTest <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[,  "Package"])]
+  if (length(new.pkg)) 
+    install.packages(new.pkg,  dependencies = TRUE)
+  sapply(pkg,  require,  character.only = TRUE)
+}
+
+# here is where you load any necessary packages
+# ex: stringr
+# lapply(c("stringr"),  pkgTest)
+
+lapply(c("eha", "survival", "stargazer"),  pkgTest)
+
+
+
+
+data(child)
+
+child_surv <- with(child, Surv(enter, exit, event))
+summary(child_surv)
+
+cox <- coxph(child_surv ~ sex + m.age, data = child)
+summary(cox)
+drop1(cox, test = "Chisq")
+stargazer(cox, type = "latex")
+
+
+
